03f622aeec14eb8315c8a46e5eda3d29190e7480
אודיה סעדון <osadoun@g.jct.ac.il>
2023-02-28 23:24:48 +0200

    pages spa

--- js/client/login.js
+++ js/client/login.js
@@ -1,40 +1,40 @@
-import {Page} from '../client/pages.js'
+import { page } from '../client/pages.js'
 
 function submitLogin() {
 
     let email_login = document.getElementById('email').value;
     let password_login = document.getElementById('password').value;
 
-    let logged_user = {email: "", password: "", fname: "", lname: "", phone: ""};
+    let logged_user = { email: "", password: "", fname: "", lname: "", phone: "" };
 
 
     var req = new FXMLhttpRequest();
     req.open(
-     'GET',
-     'server_fulstack3/submitLogin',
-     {email : email_login, password : password_login},
-     function(response) {
-        console.log(response)
-        if (response.status === 200){
-            //200 = ok
-            var user = response.user;
-            logged_user = {
-                email : user.email,
-                password: user.password,
-                fname : user.first_name,
-                lname : user.last_name, 
-                phone : user.phone
-            };
+        'GET',
+        'server_fulstack3/submitLogin',
+        { email: email_login, password: password_login },
+        function (response) {
+            console.log(response)
+            if (response.status === 200) {
+                //200 = ok
+                var user = response.user;
+                logged_user = {
+                    email: user.email,
+                    password: user.password,
+                    fname: user.first_name,
+                    lname: user.last_name,
+                    phone: user.phone
+                };
 
-            Page.hideLogin();
-            Page.hideSignup();
-            Page.hideAddMealToMenu();
-            Page.hideUpdateMeal();
+                page.hideLogin();
+                page.hideSignup();
+                page.hideAddMealToMenu();
+                page.hideUpdateMeal();
 
-            console.log('show menu')
-            showMenu();
-        }
-    });
+                console.log('show menu')
+                showMenu();
+            }
+        });
     req.send();
 
 
@@ -54,7 +54,7 @@ function submitLogin() {
 
 }
 
-function sign_in(event){ 
+function sign_in(event) {
     // get password and username from login form
     var userElement = document.getElementById("UsernameText");
     var user_username = userElement.value;
@@ -63,27 +63,27 @@ function sign_in(event){
 
     var req = new FXMLhttpRequest();
     req.open(
-     'GET',
-     'server.com/SignIn',
-     {username : user_username, password : user_password},
-     function(response) {
-        console.log(response)
-        if (response.status === 200){
-            
-            var user = response.user;
-            logged_user = {
-                username : user.username,
-                password: user.password,
-                id : user.id,
-                fname : user.fname,
-                lname : user.lname
-            };
+        'GET',
+        'server.com/SignIn',
+        { username: user_username, password: user_password },
+        function (response) {
+            console.log(response)
+            if (response.status === 200) {
 
-            exit_login_page();
-            exit_signup_page();
-            console.log('log in success, loading tasks...');
-            load_todo_list();
-        }
-    });
+                var user = response.user;
+                logged_user = {
+                    username: user.username,
+                    password: user.password,
+                    id: user.id,
+                    fname: user.fname,
+                    lname: user.lname
+                };
+
+                exit_login_page();
+                exit_signup_page();
+                console.log('log in success, loading tasks...');
+                load_todo_list();
+            }
+        });
     req.send();
 }
--- js/client/pages.js
+++ js/client/pages.js
@@ -1,3 +1,10 @@
+window.onload = function () {
+    console.log("load page");
+    showHomePage();
+    console.log("after load page");
+
+};
+
 function hideLogin(){
     document.getElementById('login').style.display = 'none';
 }
@@ -18,28 +25,158 @@ function hideUpdateMeal(){
     document.getElementById('update_meal').style.display = 'none';
 }
 
+function hideMainPage(){
+    console.log("hide home");
+    document.getElementById('home').style.display = 'none';
+}
+
 function showLogin() {
     var clon = document.getElementById('login').content.cloneNode(true);
+    document.body.removeChild(document.body.lastElementChild);
     document.body.appendChild(clon);
 }
 
 function showSignup() {
     var clon = document.getElementById('signup').content.cloneNode(true);
+    document.body.removeChild(document.body.lastElementChild);
     document.body.appendChild(clon);
 }
 
 function showMenu() {
     var clon = document.getElementById('menu').content.cloneNode(true);
+    document.body.removeChild(document.body.lastElementChild);
     document.body.appendChild(clon);
 }
 
 function showAddMealToMenu() {
     var clon = document.getElementById('add_meal_to_menu').content.cloneNode(true);
+    document.body.removeChild(document.body.lastElementChild);
     document.body.appendChild(clon);
 }
 
 function showUpdateMeal() {
     var clon = document.getElementById('update_meal').content.cloneNode(true);
+    document.body.removeChild(document.body.lastElementChild);
     document.body.appendChild(clon);
 }
+
+function showMain(){
+    document.getElementById('home').style.display = 'display';
+    document.body.removeChild(document.body.lastElementChild);
+}
+
+function showHomePage(){
+    console.log("mainpage");
+
+    document.getElementById('button_nav_logout').style.display = 'none';
+    document.getElementById('button_nav_menu').style.display = 'none';
+    document.getElementById('button_nav_add_meal').style.display = 'none';
+    document.getElementById('button_nav_update_menu').style.display = 'none';
+
+    hideLogin();
+    hideSignup();
+    hideMenu();
+    hideAddMealToMenu();
+    hideUpdateMeal();
+
+    showMain();
+}
+
+function showLoginPage(){
+    console.log("loginpage");
+
+    document.getElementById('button_nav_logout').style.display = 'none';
+    document.getElementById('button_nav_menu').style.display = 'none';
+    document.getElementById('button_nav_add_meal').style.display = 'none';
+    document.getElementById('button_nav_update_menu').style.display = 'none';
+
+    hideMainPage();
+    hideSignup();
+    hideMenu();
+    hideAddMealToMenu();
+    hideUpdateMeal();
+
+    showLogin();
+}
+
+function showSignupPage(){
+    console.log("signuppage");
+
+    document.getElementById('button_nav_logout').style.display = 'none';
+    document.getElementById('button_nav_menu').style.display = 'none';
+    document.getElementById('button_nav_add_meal').style.display = 'none';
+    document.getElementById('button_nav_update_menu').style.display = 'none';
+
+    hideMainPage();
+    hideLogin();
+    hideMenu();
+    hideAddMealToMenu();
+    hideUpdateMeal();
+
+    showSignup();
+}
+
+function showMenuPage(){
+    console.log("menupage");
+
+    document.getElementById('button_nav_login').style.display = 'none';
+    document.getElementById('button_nav_signup').style.display = 'none';
+
+
+    document.getElementById('button_nav_logout').style.display = 'display';
+    document.getElementById('button_nav_menu').style.display = 'display';
+    document.getElementById('button_nav_add_meal').style.display = 'display';
+    document.getElementById('button_nav_update_menu').style.display = 'display';
+
+    hideMainPage();
+    hideLogin();
+    hideSignup();
+    hideAddMealToMenu();
+    hideUpdateMeal();
+
+    showMenu();
+}
+
+function showAddMealToMenuPage(){
+    console.log("addmealpage");
+
+    document.getElementById('button_nav_login').style.display = 'none';
+    document.getElementById('button_nav_signup').style.display = 'none';
+
+
+    document.getElementById('button_nav_logout').style.display = 'display';
+    document.getElementById('button_nav_menu').style.display = 'display';
+    document.getElementById('button_nav_add_meal').style.display = 'display';
+    document.getElementById('button_nav_update_menu').style.display = 'display';
+
+    hideMainPage();
+    hideLogin();
+    hideSignup();
+    hideMenu();
+    hideUpdateMeal();
+
+    showAddMealToMenu();
+}
+
+function showUpdateMealPage(){
+    console.log("signuppage");
+
+    document.getElementById('button_nav_login').style.display = 'none';
+    document.getElementById('button_nav_signup').style.display = 'none';
+
+
+    document.getElementById('button_nav_logout').style.display = 'display';
+    document.getElementById('button_nav_menu').style.display = 'display';
+    document.getElementById('button_nav_add_meal').style.display = 'display';
+    document.getElementById('button_nav_update_menu').style.display = 'display';
+
+    hideMainPage();
+    hideLogin();
+    hideSignup();
+    hideMenu();
+    hideAddMealToMenu();
+
+    showUpdateMeal();
+}
+
 
