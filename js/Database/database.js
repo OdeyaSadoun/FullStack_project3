76fa62a124a714711e42e16902272f62451ab1d8
אודיה סעדון <osadoun@g.jct.ac.il>
2023-03-05 01:07:36 +0200

    add database

---
+++ js/Database/database.js
@@ -0,0 +1,23 @@
+export class Database {
+    static loadUsers() {
+        let usersFromStroge = JSON.parse(window.localStorage.getItem('users'));
+        if (usersFromStroge) {
+            return usersFromStroge;
+        }
+        else {
+            window.localStorage.setItem('users', '{}');
+            return {};
+        }
+    }
+    static loadMeals() {
+        let mealsFromStroge = JSON.parse(window.localStorage.getItem('meals'));
+        if (mealsFromStroge) {
+
+            return mealsFromStroge;
+        }
+        else {
+            window.localStorage.setItem('meals', '{}');
+            return {};
+        }
+    }
+}
--- js/client/login.js
+++ js/client/login.js
@@ -4,24 +4,23 @@ import {FXMLhttpRequest} from '../FXMLHttpRequest/FXMLHttpRequest.js'
 
 export function submitLogin() {
 
-    console.log(1);
     let email_login = document.getElementById('email').value;
     let password_login = document.getElementById('password').value;
-    console.log(2);
+
     let logged_user = { email: "", password: "", fname: "", lname: "", phone: "" };
 
-    console.log(3);
+ 
     var req = new FXMLhttpRequest();
-    console.log(4);
+
     req.onload = function (response) {
-        console.log('go to server');
+        console.log('login- go to server');
         console.log(response);
         if (response.status === 200) {
             //200 = ok
             var user = response.user;
             logged_user = {
-                email: user.email,
-                password: user.password,
+                email: email_login,
+                password: password_login,
                 fname: user.first_name,
                 lname: user.last_name,
                 phone: user.phone
@@ -33,7 +32,7 @@ export function submitLogin() {
     };
     req.open(
         'POST',
-        'server_fulstack3/submitLogin',
+        'server_fullstack3/getUser',
         { email: email_login, password: password_login });
     req.send();
 
