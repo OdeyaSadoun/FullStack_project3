6375b6fdb0e018001a49ce4ddc6876c3b2dda331
אודיה סעדון <osadoun@g.jct.ac.il>
2023-03-06 15:30:59 +0200

    more in db

--- js/client/managemaels.js
+++ js/client/managemaels.js
@@ -1,12 +1,11 @@
-
-
+import { getLoggedUser } from "./login";
 
 export function addMealToMenu(){
     var req = new FXMLhttpRequest();
     req.open(
         'PUT',
-        'server_fulstack3/submitSignup',
-        { email: email_signup, password: password_signup, first_name: fname, last_name: lname, phone: phone_signup },
+        'server_fulstack3/addMeal',
+        {meal : {name:name} , user: {email : getLoggedUser().email }},
         function (response) {
             console.log(response);
             if (response.status === 200) {
